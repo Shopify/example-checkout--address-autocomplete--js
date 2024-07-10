708fae693fe098a03fbe153c0cb2faf1cd5b1428
Hayley Dixon <78764587+hheyhhay@users.noreply.github.com>
2024-07-10 13:34:51 -0400

    add query params to fetch

--- extensions/example-checkout-address-autocomplete-js/src/suggest.js
+++ extensions/example-checkout-address-autocomplete-js/src/suggest.js
@@ -2,16 +2,19 @@ import { extension } from "@shopify/ui-extensions/checkout";
 
 export default extension(
   "purchase.address-autocomplete.suggest",
-  async ({ signal }) => {
+  async ({ signal, target }) => {
+    const { field, value } = target;
+
     const response = await fetch(
-      `https://hheyhhay.github.io/address-autocomplete/address-autocomplete.json`,
+      `https://hheyhhay.github.io/address-autocomplete/address-autocomplete.json?query=${value}&field=${signal}`,
       {
         signal,
       }
     );
-    const { data } = await response.json();
 
-    const suggestions = data.map((suggestion) => {
+    const { result } = await response.json();
+
+    const suggestions = result.suggestions.map((suggestion) => {
       return {
         id: suggestion.global_address_key,
         label: suggestion.text,
